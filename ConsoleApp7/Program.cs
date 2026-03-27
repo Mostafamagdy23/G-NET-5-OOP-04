c9818ba1ed13359d2801fac6b22d8ff296a5124b
Mostafamagdy23 <mostafamagdy.per@gmail .com>
2026-03-27 22:03:37 +0200

    Part01 Theoretical Questions

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 - Theoretical Answers
+
+            // Q1: Difference between static binding and dynamic binding?
+
+            // Static Binding:
+            // Happens at compile time.
+            // Used with method overloading.
+
+            // Dynamic Binding:
+            // Happens at runtime.
+            // Used with method overriding and virtual methods.
+
+
+
+            // Q2: Difference between method overloading and method overriding?
+
+            // Method Overloading:
+            // Same method name with different parameters.
+            // Happens in the same class.
+            // Compile time.
+
+            // Method Overriding:
+            // Same method name and parameters.
+            // Happens between base and child class.
+            // Runtime.
+
+
+
+            // Q3: Keywords used for Method Overriding?
+
+            // virtual:
+            // Used in base class to allow overriding.
+
+            // override:
+            // Used in child class to change base method.
+
+            // base:
+            // Used to call base class method.
+
+            #endregion
         }
     }
 }
